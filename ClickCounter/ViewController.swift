95bd527c6da9550cb3ad5f207698688a7869d87e
royFloresNyc <royfloresnyc@gmail.com>
2021-12-28 13:01:14 -0500

    Create decrement button and background color toggler

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -11,9 +11,12 @@ class ViewController: UIViewController {
 
     var count = 0
     var label: UILabel!
+    var backgroundColor = UIColor.cyan
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = backgroundColor
+
         // label
         let label = UILabel()
         label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
@@ -21,18 +24,39 @@ class ViewController: UIViewController {
         view.addSubview(label)
         self.label = label
 
-        //button
-        let button = UIButton()
-        button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
-        button.setTitle("Click", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        view.addSubview(button)
+        //increment button
+        let incrementButton = UIButton()
+        incrementButton.frame = CGRect(x: 150, y: 250, width: 100, height: 60)
+        incrementButton.setTitle("Increment", for: .normal)
+        incrementButton.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(incrementButton)
 
-        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControl.Event.touchUpInside)
+        incrementButton.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControl.Event.touchUpInside)
+
+        //decrement button
+        let decrementButton = UIButton()
+        decrementButton.frame = CGRect(x: 150, y: 350, width: 100, height: 60)
+        decrementButton.setTitle("Decrement", for: .normal)
+        decrementButton.setTitleColor(UIColor.red, for: .normal)
+        view.addSubview(decrementButton)
+
+        decrementButton.addTarget(self, action: #selector(ViewController.decrementCount), for: UIControl.Event.touchUpInside)
     }
 
     @objc func incrementCount() {
         self.count += 1
         self.label.text = "\(self.count)"
+        toggleBackgroundColor()
+    }
+
+    @objc func decrementCount() {
+        self.count -= 1
+        self.label.text = "\(self.count)"
+        toggleBackgroundColor()
+    }
+
+    func toggleBackgroundColor() {
+        backgroundColor = backgroundColor == UIColor.cyan ? UIColor.magenta : UIColor.cyan
+        view.backgroundColor = backgroundColor
     }
 }
